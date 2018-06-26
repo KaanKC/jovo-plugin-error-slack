20e90ce089aa0d5ed82ccab45f0b885e5c66a79e
Kaan Kilic <kilickaan1998@gmail.com>
2018-06-26 17:58:23 +0200

    :sparkles: User can now freely customize the message

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 const { Plugin } = require('jovo-framework');
 const request = require('request');
 
+const jovoLogo16x16URL = 'https://raw.githubusercontent.com/jovotech/jovo-framework-nodejs/master/docs/img/jovo-logo-16x16.png';
+
 class SlackError extends Plugin {
-    constructor(slackWebhookURL, options) {
+    /**
+     * ts doesn't work
+     */
+    constructor(options) {
         super(options);
-        this.slackWebhookURL = slackWebhookURL;
+        this.webhookURL = options.webhookURL;
+        this.channel = options.channel ? options.channel : "",
+        this.fallback = options.fallback ? options.fallback : "Error Message";
+        this.color = options.color ? options.color : "#ff0000";
+        this.pretext = options.pretext ? options.pretext : "" 
+        this.author_name = options.author_name ? options.author_name : ""
+        this.author_link = options.author_link ? options.author_link : "";
+        this.author_icon = options.author_icon ? options.author_icon : "";
+        this.title = options.title ? options.title : "An error has occured!",
+        this.title_link = options.title_link ? options.title_link : "";
+        this.text = options.text ? options.text : "";
+        this.image_url = options.image_url ? options.image_url : "";
+        this.thumb_url = options.thumb_url ? options.thumb_url : "";
+        this.footer = options.footer ? options.footer : "Jovo Plugin - Slack Error";
+        this.footer_icon = options.footer_icon ? options.footer_icon : jovoLogo16x16URL;
     }
 
     init() {
         const { app } = this;
         app.on('handlerError', (jovo, err) => {
             let log = this.createLog(jovo, 'Handler Error', err);
-            this.sendRequest(this.slackWebhookURL, log);
+            this.sendRequest(log);
         });
 
         app.on('responseError', (jovo, err) => {
+            console.log('kek');
             let log = this.createLog(jovo, 'Response Error', err);
-            this.sendRequest(this.slackWebhookURL, log);
+            this.sendRequest(log);
         });
     }
 
@@ -26,13 +46,23 @@ class SlackError extends Plugin {
         * @returns {*}
         */
     createLog(jovo, errorType, err) {
-        const ts = jovo.getTimestamp();
         let log = {
+            "channel": this.channel,
             "attachments": [
                 {
-                    "fallback": "Error message",
-                    "color": "#ff0000",
-                    "title": "An error has occured!",
+                    "fallback": this.fallback,
+                    "color": this.color,
+                    "pretext": this.pretext,
+                    "author_name": this.author_name,
+                    "author_link": this.author_link,
+                    "author_icon": this.author_icon,
+                    "title": this.title,
+                    "title_link": this.title_link,
+                    "text": this.text,
+                    "image_url": this.image_url,
+                    "thumb_url": this.thumb_url,
+                    "footer": this.footer,
+                    "footer_icon": this.footer_icon,
                     "fields": [
                         {
                             "title": "UserID",
@@ -41,7 +71,7 @@ class SlackError extends Plugin {
                         },
                         {
                             "title": "Timestamp",
-                            "value": ts,
+                            "value": jovo.getTimestamp(),
                             "short": true
                         },
                         {
@@ -74,24 +104,23 @@ class SlackError extends Plugin {
                             "value": err.stack.toString(),
                             "short": false
                         }
-                    ],
-                    "image_url": "http://my-website.com/path/to/image.jpg",
-                    "thumb_url": "http://example.com/path/to/thumb.png",
-                    "footer": "Jovo Error Plugin",
-                    "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
-                    "ts": (new Date(ts)).getTime()
+                    ]
                 }
             ]
         }
         return log;
     }
 
-    sendRequest(url, body) {
+    sendRequest(body) {
         request({
-            url: url,
+            url: this.webhookURL,
             method: 'POST',
             json: true,
             body: body
+        }, function (error, response, body) {
+            if (error) {
+                console.log(error);
+            }
         });
     }
 }
