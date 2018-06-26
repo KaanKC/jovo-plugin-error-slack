ced4274668f0a52b6d9e533946080e767c1c4f49
Kaan Kilic <kilickaan1998@gmail.com>
2018-06-26 18:17:05 +0200

    :art: Improves constructor variable assignment

--- index.js
+++ index.js
@@ -7,20 +7,20 @@ class SlackError extends Plugin {
     constructor(options) {
         super(options);
         this.webhookURL = options.webhookURL;
-        this.channel = options.channel ? options.channel : '',
-        this.fallback = options.fallback ? options.fallback : 'Error Message';
-        this.color = options.color ? options.color : '#ff0000';
-        this.pretext = options.pretext ? options.pretext : '' 
-        this.author_name = options.author_name ? options.author_name : ''
-        this.author_link = options.author_link ? options.author_link : '';
-        this.author_icon = options.author_icon ? options.author_icon : '';
-        this.title = options.title ? options.title : 'An error has occured!',
-        this.title_link = options.title_link ? options.title_link : '';
-        this.text = options.text ? options.text : '';
-        this.image_url = options.image_url ? options.image_url : '';
-        this.thumb_url = options.thumb_url ? options.thumb_url : '';
-        this.footer = options.footer ? options.footer : 'Jovo Plugin - Slack Error';
-        this.footer_icon = options.footer_icon ? options.footer_icon : jovoLogo16x16URL;
+        this.channel = options.channel || '',
+        this.fallback = options.fallback || 'Error Message';
+        this.color = options.color || '#ff0000';
+        this.pretext = options.pretext || '' 
+        this.author_name = options.author_name || ''
+        this.author_link = options.author_link || '';
+        this.author_icon = options.author_icon || '';
+        this.title = options.title || 'An error has occured!',
+        this.title_link = options.title_link || '';
+        this.text = options.text || '';
+        this.image_url = options.image_url || '';
+        this.thumb_url = options.thumb_url || '';
+        this.footer = options.footer || 'Jovo Plugin - Slack Error';
+        this.footer_icon = options.footer_icon || jovoLogo16x16URL;
     }
 
     init() {
