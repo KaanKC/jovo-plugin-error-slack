6ad8b863fb5f869afef68a5bc94aef0094651bf6
Kaan Kilic <kilickaan1998@gmail.com>
2019-02-05 11:33:25 +0100

    :sparkles: Adds logs for SessionEndedRequest.ERROR

--- src/SlackErrorPlugin.ts
+++ src/SlackErrorPlugin.ts
@@ -52,22 +52,94 @@ export class SlackErrorPlugin implements Plugin {
      * @param app 
      */
     install(app: BaseApp) {
-        app.middleware('fail')!.use(this.log.bind(this));
+        // errors
+        app.middleware('fail')!.use(this.error.bind(this));
+        // SessionEndedRequest
+        app.middleware('platform.nlu')!.use(this.sessionEndedRequest.bind(this));
     }
 
     uninstall(app: BaseApp){
 
     }
+
+    sessionEndedRequest(handleRequest: HandleRequest) {
+        if (!handleRequest.jovo) {
+            return;
+        }
+        console.log(handleRequest.jovo.$request!.toJSON().request.type);
+        if (handleRequest.jovo.$request!.toJSON().request.type === 'SessionEndedRequest') {
+            // not user initiated, i.e. there was an error
+            if (handleRequest.jovo.$request!.toJSON().request.reason === 'ERROR') {
+                const log = this.createSessionEndedLog(handleRequest);
+                this.sendRequest(log);
+            }
+        }
+    }
     
+    createSessionEndedLog(handleRequest: HandleRequest) {
+        const log = {
+            "channel": this.config.channel,
+            "attachments": [
+                {
+                    "fallback": this.config.fallback,
+                    "color": this.config.color,
+                    "pretext": this.config.pretext,
+                    "author_name": this.config.author_name,
+                    "author_link": this.config.author_link,
+                    "author_icon": this.config.author_icon,
+                    "title": this.config.title,
+                    "title_link": this.config.title_link,
+                    "text": this.config.text,
+                    "image_url": this.config.image_url,
+                    "thumb_url": this.config.thumb_url,
+                    "footer": this.config.footer,
+                    "footer_icon": this.config.footer_icon,
+                    "fields": [
+                        {
+                            "title": "RequestId",
+                            "value": handleRequest.jovo!.$request!.toJSON().request.requestId,
+                            "short": false
+                        },
+                        {
+                            "title": "Timestamp",
+                            "value": handleRequest.jovo!.$request!.getTimestamp(),
+                            "short": true
+                        },
+                        {
+                            "title": "Locale",
+                            "value": handleRequest.jovo!.$request!.getLocale(),
+                            "short": true
+                        },
+                        {
+                            "title": "Reason",
+                            "value": handleRequest.jovo!.$request!.toJSON().request.reason,
+                            "short": true
+                        },
+                        {
+                            "title": "Error Type",
+                            "value": handleRequest.jovo!.$request!.toJSON().request.error.type,
+                            "short": true
+                        },
+                        {
+                            "title": "Error Message",
+                            "value": handleRequest.jovo!.$request!.toJSON().request.error.message,
+                            "short": true
+                        }
+                    ]
+                }
+            ]
+        }
+        return log;
+    }
     /**
      * Will be called every time an error occurs
      * @param handleRequest contains current app?, host?, jovo? and error? instance
      */
-    log(handleRequest: HandleRequest): void {
+    error(handleRequest: HandleRequest): void {
         if (!handleRequest.jovo) {
             return;
         }
-        const log = this.createLog(handleRequest);
+        const log = this.createErrorLog(handleRequest);
         this.sendRequest(log);
     }
 
@@ -75,7 +147,7 @@ export class SlackErrorPlugin implements Plugin {
      * Creates message for Slack
      * @param handleRequest 
      */
-    createLog(handleRequest: HandleRequest): object {
+    createErrorLog(handleRequest: HandleRequest): object {
         const log = {
             "channel": this.config.channel,
             "attachments": [
